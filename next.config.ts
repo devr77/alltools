f13381c92294d9734fbe00768ad7e8f787f2008e
Dev Raj <draj56699@gmail.com>
2026-01-17 12:29:35 +0530

    password_gen

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",
-  ignoreBuildErrors: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
