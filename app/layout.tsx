5bf0e8a54bbab4558331d453b6aba261cbab55cf
Dev Raj <draj56699@gmail.com>
2026-01-21 10:57:40 +0530

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/site.webmanifest" />
       </head>
-      <GoogleTagManager gtmId=""GTM-N8G5XC2K />
       <body className="bg-bg text-text min-h-screen flex flex-col">
+        <GoogleTagManager gtmId="GTM-N8G5XC2K" />
         <header className="border-b border-border">
           <div className="max-w-6xl mx-auto px-4 h-14 flex items-center justify-between">
             <a className="font-semibold" href="/">
